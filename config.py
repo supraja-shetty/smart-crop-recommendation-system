4b016e05612b957478ab476a8d02afd7cf381ed6
suprajashetty <suprajashetty84@gmail.com>
2026-05-22 19:34:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Supraja@123"
-    MYSQL_DB = "crop_db"
-
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret123")
+    MYSQL_HOST = os.getenv("DB_HOST", "localhost")
+    MYSQL_USER = os.getenv("DB_USER", "root")
+    MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "Supraja@123")
+    MYSQL_DB = os.getenv("DB_NAME", "crop_db")
+    MYSQL_PORT = int(os.getenv("DB_PORT", 3306))
     PEXELS_API_KEY = os.getenv("PEXELS_API_KEY")
     EMAIL_USER = os.getenv("EMAIL_USER")
     EMAIL_PASS = os.getenv("EMAIL_PASS")
